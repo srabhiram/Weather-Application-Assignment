552dea989aae8ab8f5aeeafe60e14d4cfd6bd3b4
Abhiram <sriramoji.abhi@gmail.com>
2024-04-15 19:30:47 +0530

    Added ERror Page for routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Homepage from "./Homepage.tsx";
 import Weatherpage from "./Weatherpage.tsx";
+import ErrorPage from "./Components/ErrorPage.tsx";
 
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <Homepage />,
+      errorElement: <ErrorPage />,
     },
     {
       path: "/weather/:geoname_id",
       element: <Weatherpage />,
+      errorElement: <ErrorPage />,
+
     },
+    {
+      path: "*",
+      element: <ErrorPage/>,
+    }
   ]);
   return (
     <>
